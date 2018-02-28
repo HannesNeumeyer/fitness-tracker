970aea55978e397eccfc689408b72062c0fad364
HannesNeumeyer <hannesneu@mail.com>
2018-02-28 14:09:44 +0100

    Adding Checkbox and few styles

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
-import {MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule} from '@angular/material';
+import {MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule,
-            MatNativeDateModule],
+            MatNativeDateModule, MatCheckboxModule],
   exports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule,
-            MatNativeDateModule]
+            MatNativeDateModule, MatCheckboxModule]
 })
 
 
