773ff490f87460b9198795cf4137edde491e2a9d
HannesNeumeyer <hannesneu@mail.com>
2018-03-01 12:49:01 +0100

    Creating New Training

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
-import {MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule} from '@angular/material';
+import {MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule} from '@angular/material';
 
 @NgModule({
   imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule,
             MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule,
-            MatIconModule, MatListModule, MatTabsModule],
+            MatIconModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule,
+            MatProgressSpinnerModule, MatDialogModule],
   exports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule,
             MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule,
-            MatIconModule, MatListModule, MatTabsModule]
+            MatIconModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule,
+            MatProgressSpinnerModule, MatDialogModule]
 })
 
 
