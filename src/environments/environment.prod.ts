dc73f19c88f877a3f686f253f490bfc41844653f
HannesNeumeyer <hannesneu@mail.com>
2018-03-14 09:11:09 +0100

    Connect to Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA-g90SsZWSdIBqJ_nsArDsOrRAEmTAMOw",
+    authDomain: "oshop-dedb1.firebaseapp.com",
+    databaseURL: "https://oshop-dedb1.firebaseio.com",
+    projectId: "oshop-dedb1",
+    storageBucket: "oshop-dedb1.appspot.com",
+    messagingSenderId: "975818694994"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA-g90SsZWSdIBqJ_nsArDsOrRAEmTAMOw",
+    authDomain: "oshop-dedb1.firebaseapp.com",
+    databaseURL: "https://oshop-dedb1.firebaseio.com",
+    projectId: "oshop-dedb1",
+    storageBucket: "oshop-dedb1.appspot.com",
+    messagingSenderId: "975818694994"
+  }
 };
