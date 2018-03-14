6fedb296063d73ba4ad516f629e788b9f109e0cd
HannesNeumeyer <hannesneu@mail.com>
2018-03-14 10:36:59 +0100

    Auth with Firebase

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,45 +3,39 @@ import { AuthData } from "./auth-data.model";
 import {Subject} from 'rxjs/Subject';
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
-  private user: User;
+  private isAuthenticated = false;
 
-  constructor(private router: Router){
+  constructor(private router: Router, private afAuth: AngularFireAuth){
   }
 
   registerUser(authData: AuthData){
-    this.user = {
-      email: authData.email,
-      userId: Math.round(Math.random() * 1000).toString()
-    }
-    this.authChange.next(true);
-    this.router.navigate(['/training'])
+    this.afAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password).then(result => {
+      this.isAuthenticated = true;
+      this.authChange.next(true);
+      this.router.navigate(['/training'])
+    }).catch(err => console.log(err))
   }
 
   login(authData: AuthData){
-    this.user = {
-      email: authData.email,
-      userId: Math.round(Math.random() * 1000).toString()
-    }
-    this.authChange.next(true);
-    this.router.navigate(['/training'])
+    this.afAuth.auth.signInWithEmailAndPassword(authData.email, authData.password).then(result => {
+      this.isAuthenticated = true;
+      this.authChange.next(true);
+      this.router.navigate(['/training'])
+    }).catch(err => console.log(err))
   }
 
   logout(){
-    this.user = null;
+    this.isAuthenticated = false;
     this.authChange.next(false);
     this.router.navigate(['/login'])
   }
 
-  getUser(){
-    // for not returning the original user
-    return { ...this.user }
-  }
-
   isAuth(){
-    return this.user != null;
+    return this.isAuthenticated
   }
 }
